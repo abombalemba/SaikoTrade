9969d609930d8ca8f17d90e4c3310ef69ad70b16
abombalemba <vladan.2004@mail.ru>
2025-04-29 23:38:00 +0300

    feat #3: creating python server
    it does not work properly

---
+++ src/server.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+import uvicorn
+
+app = FastAPI()
+
+
+app.get('/')
+async def index():
+	return FileResponse('./index.html')
+
+
+if __name__ == '__main__':
+	uvicorn.run(app, host='0.0.0.0', port=8080)
